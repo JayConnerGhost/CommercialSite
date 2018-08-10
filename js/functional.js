b2818fbbfea6c7767442ee607470fd99ea50c43e
trinity martin <GhostGirl@trinitys-MacBook-Air.local>
2018-08-10 13:44:42 +0100

    adding multiple changes

---
+++ js/functional.js
@@ -0,0 +1,8 @@
+
+function openFacebookWin() {
+    window.open("https://www.facebook.com/jay.conner.73157");
+}
+//https://mobile.twitter.com/JayMart94769236
+function openTwitterWin() {
+    window.open("https://mobile.twitter.com/JayMart94769236");
+}
